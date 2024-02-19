c4bdac1f45b0bab494ee0e8b13910e13799dc537
Daniel Hara <haradaniel734@gmail.com>
2024-02-19 14:08:45 +0100

    add some padding

--- components/section/SendEmail.tsx
+++ components/section/SendEmail.tsx
@@ -1,8 +1,14 @@
 import { Button } from "../ui/button";
+import { Textarea } from '../ui/textarea'
 
 const SendEmail = () => {
   return (
-    <div>
+    <div className="p-6">
+      This is just a very simple example to automate GMail.
+      <div className="w-3/12">
+        <Textarea placeholder="Describe the email you want to send:"/>
+      </div>
+
       <Button className="m-5">
         Send email        
       </Button>
